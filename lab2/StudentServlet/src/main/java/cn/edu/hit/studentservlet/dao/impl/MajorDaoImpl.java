bcda8aaec2801f4b749b6a7ccc265ece30933e77
tb <156294753@qq.com>
2023-03-15 20:45:26 +0800

    modify DAOImpl

--- lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/impl/MajorDaoImpl.java
+++ lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/impl/MajorDaoImpl.java
@@ -1,4 +1,33 @@
-package cn.edu.hit.studentservlet.daoimpl;
+package cn.edu.hit.studentservlet.dao.impl;
 
-public class MajorDaoImpl {
+import cn.edu.hit.studentservlet.dao.MajorDao;
+import cn.edu.hit.studentservlet.entity.Major;
+
+import java.util.List;
+
+public class MajorDaoImpl implements MajorDao {
+    @Override
+    public void add(Major major) {
+
+    }
+
+    @Override
+    public void modify(Major major) {
+
+    }
+
+    @Override
+    public void remove(String mid) {
+
+    }
+
+    @Override
+    public Major getByMid(String mid) {
+        return null;
+    }
+
+    @Override
+    public List<Major> getAll(String sql) {
+        return null;
+    }
 }
--- lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/impl/StudentDaoImpl.java
+++ lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/impl/StudentDaoImpl.java
@@ -1,5 +1,93 @@
-package cn.edu.hit.studentservlet.daoimpl;
+package cn.edu.hit.studentservlet.dao.impl;
 
-public class StudentDaoImpl {
+import cn.edu.hit.studentservlet.dao.StudentDao;
+import cn.edu.hit.studentservlet.db.DBUtil;
+import cn.edu.hit.studentservlet.entity.Student;
+import com.mysql.cj.protocol.Resultset;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+public class StudentDaoImpl implements StudentDao {
+
+    @Override
+    public void add(Student stu) {
+        DBUtil db = new DBUtil();
+        String sid = stu.getSid();
+        String sname = stu.getSname();
+        String gender = stu.getGender();
+        Integer age = stu.getAge();
+        String mid = stu.getMid();
+        Date birthday = stu.getBirthday();
+
+        db.executeUpdate("INSERT INTO student(sid,sname,age,gender,mid,birthday) values" +
+                "('" + sid + "','" + sname + "','" + age + "','" + gender + "','" + mid + "','" + birthday + "')");
+
+        db.close();
+    }
+
+    /**
+     * 学号不可更改
+     * @param stu
+     */
+    @Override
+    public void modify(Student stu) {
+        DBUtil db = new DBUtil();
+        String sid = stu.getSid();
+        String sname = stu.getSname();
+        String gender = stu.getGender();
+        Integer age = stu.getAge();
+        String mid = stu.getMid();
+        Date birthday = stu.getBirthday();
+
+        db.executeUpdate("UPDATE student set sname='" + sname + "', gender='" + gender +
+                "', age='" + age + "', mid='" + mid + "', birthday='" + birthday + "',where sid='" + sid + "'");
+    }
+
+    @Override
+    public void remove(String sid) {
+        DBUtil dbUtil = new DBUtil();
+        dbUtil.executeUpdate("delete from student where sid = '" + sid + "'");
+        dbUtil.close();
+    }
+
+    /**
+     * 根据 sid 查询学生信息
+     * @param sid
+     * @return 若存在，则返回正常学生信息，否则null；
+     */
+    @Override
+    public Student getBySid(String sid) {
+        DBUtil db = new DBUtil();
+
+        ResultSet rs = db.executeQuery("select * from student where sid = '" + sid + "'");
+
+        String sname = null;
+        String gender = null;
+        Integer age = null;
+        String mid = null;
+        Date birthday = null;
+        try {
+            while(rs.next()){
+                sname = rs.getString("sname");
+                gender = rs.getString("gender");
+                age = rs.getInt("age");
+                mid = rs.getString("mid");
+                birthday = rs.getDate("birthday");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            db.close();
+
+            return new Student(sid,sname,gender,age,birthday,mid);
+        }
+    }
+
+    @Override
+    public List<Student> getAll(String sql) {
+        return null;
+    }
 }
