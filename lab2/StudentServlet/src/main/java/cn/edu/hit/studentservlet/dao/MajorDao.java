905cdf21fc4e9a986ab221498f825175fa30ccd7
tb <156294753@qq.com>
2023-03-15 20:44:54 +0800

    modify DAO

--- lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/MajorDao.java
+++ lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/MajorDao.java
@@ -1,4 +1,13 @@
 package cn.edu.hit.studentservlet.dao;
 
-public class MajorDao {
+import cn.edu.hit.studentservlet.entity.Major;
+
+import java.util.List;
+
+public interface MajorDao {
+    void add(Major major);
+    void modify(Major major);
+    void remove(String mid);
+    Major getByMid(String mid);
+    List<Major> getAll(String sql);
 }
--- lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/StudentDao.java
+++ lab2/StudentServlet/src/main/java/cn/edu/hit/studentservlet/dao/StudentDao.java
@@ -1,4 +1,13 @@
 package cn.edu.hit.studentservlet.dao;
 
-public class StudentDao {
+import cn.edu.hit.studentservlet.entity.Student;
+
+import java.util.List;
+
+public interface StudentDao {
+    void add(Student stu);
+    void modify(Student stu);
+    void remove(String sid);
+    Student getBySid(String sid);
+    List<Student> getAll(String sql);
 }
